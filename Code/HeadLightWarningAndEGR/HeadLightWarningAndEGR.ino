5e37fab00a90348db7eaf43c6cfa49a9dada4a17
Todd J. Lee <tjltm83@gmail.com>
2023-07-14 07:28:46 -0700

    some tweaks.

--- Code/HeadLightWarningAndEGR/HeadLightWarningAndEGR.ino
+++ Code/HeadLightWarningAndEGR/HeadLightWarningAndEGR.ino
@@ -24,25 +24,43 @@ void setup() {
   digitalWrite(LED, LOW);
 }
 
-void loop() {
+
+void loop(){
   bool EngineStatus = digitalRead(EngineRun);
 
-  if (digitalRead(HeadLight) == HIGH && EngineStatus == LOW) {
+  if (digitalRead(HeadLight) == LOW && digitalRead(EngineRun) == HIGH) {
+    digitalWrite(LED,HIGH);
+    digitalWrite(DingDong, HIGH);
+  }
+  else {
+    digitalWrite(LED,LOW);
+    digitalWrite(DingDong, LOW);
+  }
+  
+}
+
+
+void Oldloop() {
+  bool EngineStatus = digitalRead(EngineRun);
+
+  if (digitalRead(HeadLight) == LOW && EngineStatus == HIGH) {
+    digitalWrite(LED,HIGH);
     if (WarningDelay == false) {
       WarningDelay = true;
       WarningTimer = millis();
     }
-    if (millis() - WarningDelay) > 5000) {
+    if (millis() - WarningDelay > 5000) {
       digitalWrite(DingDong, HIGH);
     }
   }
   else {
+    digitalWrite(LED,LOW);
     digitalWrite(DingDong, LOW);
   }
 
 
-  if (EngineStatus == HIGH) {
-    if (digitalRead(EGR) == LOW) {
+  if (EngineStatus == LOW) {
+    if (digitalRead(EGR) == HIGH) {
       if (DelayEGR == false) {
         DelayEGR = true;
         EGRTimer = millis();
